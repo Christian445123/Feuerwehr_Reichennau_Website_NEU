9595b08b874e4b6a4e533ad15fa8eee2a0423634
ChrisGaming <62219497+Christian445123@users.noreply.github.com>
2026-09-15 10:37:53 +0200

    2 Dateien geaendert: assets/css/style.css includes/header.php

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Freiwillige Feuerwehr Reichenau / Innsbruck Stadt - Einsätze, Berichte, Termine und Informationen.">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Barlow+Condensed:wght@600;700;800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Barlow+Condensed:wght@600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
